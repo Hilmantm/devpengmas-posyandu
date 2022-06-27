68d8da8221d7d4953ba009749c2ef653ede8380d
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-06-27 15:00:09 +0700

    Add posyandu get child list and post new child data

--- app/src/main/java/id/kodesumsi/telkompengmas/data/source/network/NetworkService.kt
+++ app/src/main/java/id/kodesumsi/telkompengmas/data/source/network/NetworkService.kt
@@ -12,25 +12,11 @@ import retrofit2.http.POST
 
 interface NetworkService {
 
-    @POST("auth/posyandu/login")
-    fun postPosyanduLogin(
-        @Field("username") username: String,
-        @Field("password") password: String
-    ): Flowable<BaseResponse<AuthResponse>>
-
+    // =========== ORANG TUA ===========
     @POST("auth/orang-tua/login")
     fun postOrangtuaLogin(
         @Field("username") username: String,
         @Field("password") password: String
-    ): Flowable<BaseResponse<AuthResponse>>
-
-    @POST("auth/posyandu/register")
-    fun postPosyanduRegister(
-        @Field("name") name: String,
-        @Field("email") email: String,
-        @Field("password") password: String,
-        @Field("id_desa") idDesa: Int,
-        @Field("id_posyandu") idPosyandu: Int
     ): Flowable<BaseResponse<AuthResponse>>
 
     @POST("auth/orang-tua/register")
@@ -56,6 +42,41 @@ interface NetworkService {
         @Field("tinggi") height: String,
         @Field("berat") weight: String,
         @Field("lingkar_kepala") headCircumference: String
+    ): Flowable<BaseResponse<Child>>
+
+
+    // =========== POSYANDU ===========
+    @POST("auth/posyandu/login")
+    fun postPosyanduLogin(
+        @Field("username") username: String,
+        @Field("password") password: String
+    ): Flowable<BaseResponse<AuthResponse>>
+
+    @POST("auth/posyandu/register")
+    fun postPosyanduRegister(
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("password") password: String,
+        @Field("id_desa") idDesa: Int,
+        @Field("id_posyandu") idPosyandu: Int
+    ): Flowable<BaseResponse<AuthResponse>>
+
+    @GET("/api/posyandu/data-anak")
+    fun getPosyanduChildList(
+        @Header("Authorization") token: String
+    ): Flowable<BaseResponse<ListOfResponse<Child>>>
+
+    @POST("/api/posyandu/data-anak")
+    fun postOrangtuaNewChildData(
+        @Header("Authorization") token: String,
+        @Field("nama") name: String,
+        @Field("panggilan") surname: String,
+        @Field("tanggal_lahir") birthDate: String,
+        @Field("tinggi") height: String,
+        @Field("berat") weight: String,
+        @Field("lingkar_kepala") headCircumference: String,
+        @Field("nama_orang_tua") parentName: String,
+        @Field("alamat") address: String
     ): Flowable<BaseResponse<Child>>
 
 }
