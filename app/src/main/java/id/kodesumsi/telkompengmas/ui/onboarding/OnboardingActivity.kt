6f4d54a30678e64a97981a9a1e8cb0969166a0b5
Hilman Taris Muttaqin <hilmanmuttaqin345@gmail.com>
2022-06-28 17:50:11 +0700

    Fixing onboarding slider

--- app/src/main/java/id/kodesumsi/telkompengmas/ui/onboarding/OnboardingActivity.kt
+++ app/src/main/java/id/kodesumsi/telkompengmas/ui/onboarding/OnboardingActivity.kt
@@ -4,12 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.activity.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import id.kodesumsi.telkompengmas.base.BaseActivity
 import id.kodesumsi.telkompengmas.databinding.ActivityOnboardingBinding
 import id.kodesumsi.telkompengmas.ui.auth.AuthActivity
 import id.kodesumsi.telkompengmas.ui.main.MainActivity
 
+@AndroidEntryPoint
 class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>() {
+
+    private val viewModel: OnboardingViewModel by viewModels()
+
     override fun setupViewBinding(): (LayoutInflater) -> ActivityOnboardingBinding {
         return ActivityOnboardingBinding::inflate
     }
@@ -18,28 +24,41 @@ class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>() {
         binding.onboardingViewpager.adapter = OnboardingViewpagerAdapter(this)
         binding.onboardingIndicator.attachTo(binding.onboardingViewpager)
 
+        binding.onboardingViewpager.isUserInputEnabled = false
+        viewModel.onboardingCurrentItem.postValue(binding.onboardingViewpager.currentItem)
 
-        binding.nextBtn.setOnClickListener {
-            if (binding.onboardingViewpager.currentItem > 1) {
-                binding.nextBtn.text = "Mulai"
-                binding.nextBtn.setOnClickListener {
-                    // check session or check current user is login?
-                    val isLogin = false
-                    if (isLogin) {
-                        val toMain = Intent(this, MainActivity::class.java)
-                        startActivity(toMain)
-                        finish()
-                    } else {
-                        val toLogin = Intent(this, AuthActivity::class.java)
-                        startActivity(toLogin)
-                        finish()
-                    }
-                }
-            } else {
-                binding.onboardingViewpager.currentItem += 1
+        viewModel.onboardingCurrentItem.observe(this) { position ->
+            if (position == 2) {
+                setNextToAuth()
             }
         }
 
+        binding.skipBtn.setOnClickListener {
+            viewModel.onboardingCurrentItem.postValue(2)
+        }
+
+        binding.nextBtn.setOnClickListener {
+            binding.onboardingViewpager.currentItem += 1
+            viewModel.onboardingCurrentItem.postValue(binding.onboardingViewpager.currentItem)
+        }
+
+    }
+
+    private fun setNextToAuth() {
+        binding.nextBtn.text = "Mulai"
+        binding.nextBtn.setOnClickListener {
+            // check session or check current user is login?
+            val isLogin = false
+            if (isLogin) {
+                val toMain = Intent(this, MainActivity::class.java)
+                startActivity(toMain)
+                finish()
+            } else {
+                val toLogin = Intent(this, AuthActivity::class.java)
+                startActivity(toLogin)
+                finish()
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/id/kodesumsi/telkompengmas/ui/onboarding/OnboardingViewModel.kt
@@ -0,0 +1,10 @@
+package id.kodesumsi.telkompengmas.ui.onboarding
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class OnboardingViewModel: ViewModel() {
+
+    val onboardingCurrentItem = MutableLiveData<Int>()
+
+}
